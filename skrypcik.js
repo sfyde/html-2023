b08f4f6077c32e9680c7009a9adee186888024ad
fyderek <sfyderek@gmail.com>
2023-05-21 15:12:07 +0200

    lab6

---
+++ skrypcik.js
@@ -0,0 +1,9 @@
+$(function (){
+    let przycisk = $("#calculate");
+    przycisk.click(function (){
+        let inO = $("#inUnit").val();
+        let onO = $("#outUnit").val();
+        let liczba = $("#valueToConvert").val();
+        console.log(inO);
+    })
+})
